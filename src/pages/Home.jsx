ee1478563293982a7fe6d7f0b727672f5743f713
Akaash Thawani <akaashthawani123@gmail.com>
2024-12-07 12:00:31 -0500

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 import Header from "../components/Header";
 const Home = () => {
-  const heading = "E-Trading System";
+  const heading = "UniTrade";
 
   return (
     <>
