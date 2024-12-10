75050f59f8b35179b076a7ae7d2ede1f52d7cb76
Hannan Desai <hannan.desai@inventyv-git.com>
2024-12-10 14:53:07 -0500

    user srvice commit

---
+++ src/services/user.service.js
@@ -0,0 +1,12 @@
+const isUserLogIn = false;
+
+export const isLoggedIn = () => {
+    return isUserLogIn;
+}
+
+export const getUserDetail = () => {
+    return {
+        user_id: "123",
+        name: "Hannan Desai"
+    }
+}
