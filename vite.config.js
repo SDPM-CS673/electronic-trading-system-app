431d41b454d933269ea870c2542b8b0c3e2d74d8
Hannan Desai <hannan.desai@inventyv-git.com>
2024-12-11 11:32:18 -0500

    image issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  buid: {
+    assetsDir: 'assets'
+  },
   plugins: [react()],
   define: {
     "process.env": {},
