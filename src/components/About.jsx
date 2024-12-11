e9bf8fa154fac4d8511cc4b823d19ff84fdbc274
engJyotir <jn456@njit.edu>
2024-12-11 16:31:46 -0500

    landing page update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
           <div className="p-8 rounded-lg bg-gray-800 flex-1">
             <h3 className="text-2xl mb-4 text-white">About Us</h3>
             <p className="text-base leading-relaxed">
-              Unitrade is a dynamic and forward-thinking trading platform designed to cater to the needs of traders at all levels.
+            Unitrade is a dynamic and forward-thinking trading platform designed to cater to the needs of traders at all levels. Whether you are a novice looking to make your first trade or a seasoned professional, our platform is built to provide intuitive, powerful, and reliable solutions. We combine cutting-edge technology with real-time market data, advanced trading tools, and secure transaction protocols to help users navigate the financial markets with confidence. At Unitrade, we believe in democratizing access to trading and promoting financial growth and education for our users. Our goal is to build a global community of empowered traders who thrive in today's ever-evolving financial landscape.
             </p>
           </div>
   
@@ -13,13 +13,13 @@ const About = () => {
             <div className="p-8 rounded-lg bg-gray-800">
               <h3 className="text-2xl mb-4 text-white">Vision</h3>
               <p className="text-base leading-relaxed">
-                To revolutionize global financial markets by empowering traders of all levels with cutting-edge technology.
+              To revolutionize global financial markets by empowering traders of all levels with cutting-edge technology, seamless access, and data-driven insights for smarter, faster, and more secure trading experiences
               </p>
             </div>
             <div className="p-8 rounded-lg bg-gray-800">
               <h3 className="text-2xl mb-4 text-white">Mission</h3>
               <p className="text-base leading-relaxed">
-                At Unitrade, our mission is to provide an innovative and user-centric trading platform.
+              At Unitrade, our mission is to provide an innovative and user-centric trading platform that simplifies the complexities of financial markets. We aim to offer a comprehensive suite of tools that enhance market access, facilitate informed decision-making, and deliver exceptional trading opportunities for both beginners and professionals. Our commitment is to ensure transparency, security, and a seamless user experience while promoting financial literacy and inclusion worldwide.
               </p>
             </div>
           </div>
