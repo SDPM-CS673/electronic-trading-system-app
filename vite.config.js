8ec21b72e3bdeb315a4da827b8ed102bff798321
Hannan Desai <hannan.desai@inventyv-git.com>
2024-12-11 12:22:00 -0500

    rout page issue

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  buid: {
-    assetsDir: 'assets'
-  },
   plugins: [react()],
-  define: {
-    "process.env": {},
-  },
-  
 })
